62f0329b2b8250086cd936307768e404ecb9028f
KW. <117881314+karwenglim@users.noreply.github.com>
2026-07-04 17:36:17 +0800

    Add: sort-an-array - submission-0

---
+++ Data Structures & Algorithms/sort-an-array/submission-0.java
@@ -0,0 +1,68 @@
+class Solution {
+    public int[] sortArray(int[] nums) {
+        nums = mergeSort(nums);
+        return nums;
+    }
+
+    public int[] mergeSort(int[] nums) {
+        if(nums.length <= 1) {
+            return nums;
+        }
+
+        int length = nums.length;
+        int middle = length / 2;
+
+        int leftPtr = 0, rightPtr = 0;
+
+        int[] leftArr = new int [middle];
+        int[] rightArr = new int [length - middle];
+
+        for(int i = 0 ; i < nums.length ; i ++){
+            if(i < middle){
+                leftArr[leftPtr] = nums[i];
+                leftPtr ++;
+            } else {
+                rightArr[rightPtr] = nums[i];
+                rightPtr ++;
+            }
+        }
+
+        leftArr = mergeSort(leftArr);
+        rightArr = mergeSort(rightArr);
+        return merge(leftArr, rightArr, nums);
+    }
+
+    public int[] merge(int[] leftArr, int[] rightArr, int[] oriArr) {
+        int leftPtr = 0, rightPtr = 0, oriPtr = 0;
+        int leftLength = leftArr.length, rightLength = rightArr.length;
+
+        while(leftPtr < leftLength && rightPtr < rightLength) {
+            int leftNum = leftArr[leftPtr];
+            int rightNum = rightArr[rightPtr];
+
+            if(leftNum < rightNum) {
+                oriArr[oriPtr] = leftNum;
+                oriPtr ++;
+                leftPtr ++;
+            } else {
+                oriArr[oriPtr] = rightNum;
+                oriPtr ++;
+                rightPtr ++;
+            }
+        }
+
+        while(leftPtr < leftLength) {
+            oriArr[oriPtr] = leftArr[leftPtr];
+            oriPtr ++;
+            leftPtr ++;
+        }
+
+        while(rightPtr < rightLength) {
+            oriArr[oriPtr] = rightArr[rightPtr];
+            oriPtr++;
+            rightPtr ++;
+        }
+
+        return oriArr;
+    }
+}
